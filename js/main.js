cc43845dd1c7af3c1163a280893a343cd658129c
luca betti sorbelli <luca betti sorbelli>
2023-04-13 16:52:48 +0200

    prezzi scontati

--- js/main.js
+++ js/main.js
@@ -2,10 +2,29 @@ let KmDaFare = prompt ("quanti Kilometri vuoi percorrere?");
 let etaUtente = prompt ("quanti anni hai?");
 
 KmDaFare = parseInt(KmDaFare);
-etaUtente = parseInt(etaUtente)
+etaUtente = parseInt(etaUtente);
 
 //prezzo biglietto senza sconti
 
 let prezzoPerKm = 0.21;
-let prezzoBiglietto = prezzoPerKm * KmDaFare;
-alert(`il prezzo del biglietto è: ${prezzoBiglietto + "€"}`)
+let prezzoSenzaSconti = prezzoPerKm * KmDaFare;
+
+// sconti 
+
+if (etaUtente < 18) {
+    scontoBambini = 0.20 * prezzoSenzaSconti;
+    prezzoScontatoMinori = prezzoSenzaSconti - scontoBambini;
+
+alert(`prezzo ridotto under 18: ${prezzoScontatoMinori  + "€"}`);
+
+} else if(etaUtente >= 65) {
+    scontoAnziani = 0.40 * prezzoSenzaSconti;
+    prezzoScontatoAnziani = prezzoSenzaSconti - scontoAnziani;
+
+    alert(`prezzo ridotto over 65: ${prezzoScontatoAnziani  + "€"}`);
+}
+
+
+// prezzo intero
+alert(`prezzo intero: ${prezzoSenzaSconti  + "€"}`);
+    
