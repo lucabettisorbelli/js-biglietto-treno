6e6b694a812fdbf4f2ffad8f87882a573a1569fc
luca betti sorbelli <luca betti sorbelli>
2023-04-13 15:15:11 +0200

    parseInt

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-let KmDaFare = prompt ("quanti Kilometri vuoi percorrere?")
-let etaUtente = prompt ("quanti anni hai?")
+let KmDaFare = prompt ("quanti Kilometri vuoi percorrere?");
+let etaUtente = prompt ("quanti anni hai?");
+
+KmDaFare = parseInt(KmDaFare);
+etaUtente = parseInt(etaUtente)
+
+//prezzo biglietto 
+
+let prezzoBiglietto
+
