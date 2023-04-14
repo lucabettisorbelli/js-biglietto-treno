6d1d13e723b5464eae216f9aefe9c89a653a27e1
luca betti sorbelli <luca betti sorbelli>
2023-04-14 10:11:25 +0200

    aggiunta let

--- js/main.js
+++ js/main.js
@@ -14,16 +14,16 @@ let prezzoSenzaSconti = prezzoPerKm * KmDaFare;
 if (etaUtente < 18) {
 
     //sconto under 18
-    scontoBambini = 0.20 * prezzoSenzaSconti;
-    prezzoScontatoMinori = prezzoSenzaSconti - scontoBambini;
+    let scontoBambini = 0.20 * prezzoSenzaSconti;
+    let prezzoScontatoMinori = prezzoSenzaSconti - scontoBambini;
 
     alert(`prezzo ridotto under 18: ${prezzoScontatoMinori.toFixed(2)  + "€"}`);
 
 } else if(etaUtente >= 65) {
 
     //sconto over 65
-    scontoAnziani = 0.40 * prezzoSenzaSconti;
-    prezzoScontatoAnziani = prezzoSenzaSconti - scontoAnziani;
+    let scontoAnziani = 0.40 * prezzoSenzaSconti;
+    let prezzoScontatoAnziani = prezzoSenzaSconti - scontoAnziani;
 
     alert(`prezzo ridotto over 65: ${prezzoScontatoAnziani.toFixed(2)  + "€"}`);
 
